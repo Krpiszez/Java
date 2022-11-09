b0524c11eeb5eb30267766b71cd0955a31aae9a6
Omer Isik <113728431+Krpiszez@users.noreply.github.com>
2022-11-09 23:37:48 +0100

    Add files via upload

---
+++ src/main/java/CompetetiveCoding/LongestPalindrome.java
@@ -0,0 +1,9 @@
+package CompetetiveCoding;
+/*
+        Given a string s which consists of lowercase or uppercase letters,
+         return the length of the longest palindrome that can be built with those letters.
+
+        Letters are case-sensitive, for example, "Aa" is not considered a palindrome here.
+ */
+public class LongestPalindrome {
+}
