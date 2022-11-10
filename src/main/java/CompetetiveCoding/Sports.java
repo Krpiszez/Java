9d40175b4e855b3007f99766ae257d159925dd44
Omer Isik <113728431+Krpiszez@users.noreply.github.com>
2022-11-10 19:35:12 +0100

    Add files via upload

---
+++ src/main/java/CompetetiveCoding/Sports.java
@@ -0,0 +1,12 @@
+package CompetetiveCoding;
+
+class Sports{
+
+    String getName(){
+        return "Generic Sports";
+    }
+
+    void getNumberOfTeamMembers(){
+        System.out.println( "Each team has n players in " + getName() );
+    }
+}
