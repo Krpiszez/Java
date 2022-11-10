5ad7bf9880217b2c3656fbf87cb7478a0faedd2e
Omer Isik <113728431+Krpiszez@users.noreply.github.com>
2022-11-10 19:34:54 +0100

    Add files via upload

---
+++ src/main/java/CompetetiveCoding/Soccer.java
@@ -0,0 +1,20 @@
+package CompetetiveCoding;
+
+class Soccer extends Sports{
+    @Override
+    String getName(){
+        return "Soccer Class";
+    }
+
+    @Override
+    void getNumberOfTeamMembers() {
+        super.getNumberOfTeamMembers();
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+
+
+}
