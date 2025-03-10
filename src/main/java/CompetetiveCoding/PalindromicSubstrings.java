7c997a007e9ccb87d67f977843768b7793699ed5
Omer Isik <isikomer4545@gmail.com>
2025-03-10 10:29:23 +0000

    Coding exercise

---
+++ src/main/java/CompetetiveCoding/PalindromicSubstrings.java
@@ -0,0 +1,53 @@
+package CompetetiveCoding;
+
+import java.util.Arrays;
+
+/*
+        Given a string s which consists of lowercase or uppercase letters,
+         return the length of the longest palindrome that can be built with those letters.
+
+        Letters are case-sensitive, for example, "Aa" is not considered a palindrome here.
+ */
+public class PalindromicSubstrings {
+
+    static int resultStart;
+    static int resultLength;
+    static int count;
+
+    public static String longestPalindrome(String s) {
+
+        int length = s.length();
+
+        if (length < 2) {
+            return s;
+        }
+
+        for (int start = 0; start < length; start++) {
+            expandRange(s, start, start);
+            expandRange(s, start, start + 1);
+        }
+        return s.substring(resultStart, resultStart + resultLength);
+    }
+
+    private static void expandRange(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+            count++;
+        }
+        if (resultLength < right - left -1) {
+            resultStart = left + 1;
+            resultLength = right - left -1;
+        }
+    }
+
+
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4};
+
+        System.out.println(Arrays.toString(arr));
+        System.out.println(longestPalindrome("aaa"));
+        System.out.println(count);
+    }
+
+}
