b06f37c97f8c98ef88abcad12098c12220731535
Omer Isik <isikomer4545@gmail.com>
2025-03-29 14:51:27 +0000

    Coding exercise

--- src/main/java/CompetetiveCoding/AllConstruct.java
+++ src/main/java/CompetetiveCoding/AllConstruct.java
@@ -9,13 +9,20 @@ public class AllConstruct {
     public static void main(String[] args) {
         AllConstruct canConstruct = new AllConstruct();
 
-        System.out.println(canConstruct.canConstruct("purple", new String[]{"purp", "p", "ur", "le", "purpl", "sk", "boar"}));
-        System.out.println(canConstruct.canConstruct("abcdef", new String[]{"ab", "abc", "cd", "def", "abcd", "ef", "c"}));
-        System.out.println(canConstruct.canConstruct("skateboard", new String[]{"bo", "rd", "ate", "t", "ska", "sk", "boar"}));
-        System.out.println(canConstruct.canConstruct("aaaaaaaaaaaaaaaaaaaaaaaaaaaz", new String[]{"a", "aa", "aaa", "aaaa", "aaaaa", "aaaaaa", "aaaaaaa"}));
+        System.out.println(canConstruct.allConstruct("purple", new String[]{"purp", "p", "ur", "le", "purpl", "sk", "boar"}));
+        System.out.println(canConstruct.allConstructTabulation("purple", new String[]{"purp", "p", "ur", "le", "purpl", "sk", "boar"}));
+        memo.clear();
+        System.out.println(canConstruct.allConstruct("abcdef", new String[]{"ab", "abc", "cd", "def", "abcd", "ef", "c"}));
+        System.out.println(canConstruct.allConstructTabulation("abcdef", new String[]{"ab", "abc", "cd", "def", "abcd", "ef", "c"}));
+        memo.clear();
+        System.out.println(canConstruct.allConstruct("skateboard", new String[]{"bo", "rd", "ate", "t", "ska", "sk", "boar"}));
+        System.out.println(canConstruct.allConstructTabulation("skateboard", new String[]{"bo", "rd", "ate", "t", "ska", "sk", "boar"}));
+        memo.clear();
+        System.out.println(canConstruct.allConstruct("aaaaaaaaaaaaaaaaaaaaaaaaaaaz", new String[]{"a", "aa", "aaa", "aaaa", "aaaaa", "aaaaaa", "aaaaaaa"}));
+        System.out.println(canConstruct.allConstructTabulation("aaaaaaaaaaz", new String[]{"a", "aa", "aaa", "aaaa", "aaaaa", "aaaaaa", "aaaaaaa"}));
     }
 
-    private List<List<String>> canConstruct(String abcdef, String[] strings) {
+    private List<List<String>> allConstruct(String abcdef, String[] strings) {
 
         if (memo.containsKey(abcdef)) return memo.get(abcdef);
         if (abcdef.isEmpty()) {
@@ -29,7 +36,7 @@ public class AllConstruct {
         for (String str : strings) {
             if (abcdef.startsWith(str)) {
                 String target = abcdef.substring(str.length());
-                List<List<String>> suffixWays = canConstruct(target, strings);
+                List<List<String>> suffixWays = allConstruct(target, strings);
                 for (List<String> way : suffixWays) {
                     List<String> newCombination = new ArrayList<>();
                     newCombination.add(str);
@@ -41,5 +48,30 @@ public class AllConstruct {
 
         memo.put(abcdef, result);
         return result;
+    }
+
+    private List<List<String>> allConstructTabulation(String abcdef, String[] strings) {
+
+        List<List<List<String>>> table = new ArrayList<>();
+        for (int i = 0; i <= abcdef.length(); i++) {
+            table.add(new ArrayList<>());
+        }
+        table.get(0).add(new ArrayList<>());
+
+        for (int i = 0; i <= abcdef.length(); i++) {
+            for (String str : strings) {
+                if (i + str.length() <= abcdef.length() &&
+                        abcdef.startsWith(str, i)) {
+                    List<List<String>> newCombinations = new ArrayList<>();
+                    for (List<String> tab : table.get(i)) {
+                        List<String> newCombo = new ArrayList<>(tab);
+                        newCombo.add(str);
+                        newCombinations.add(newCombo);
+                    }
+                    table.get(i + str.length()).addAll(newCombinations);
+                }
+            }
+        }
+        return table.get(abcdef.length());
     }
 }
--- src/main/java/CompetetiveCoding/SearchInsert.java
+++ src/main/java/CompetetiveCoding/SearchInsert.java
@@ -12,8 +12,21 @@ public class SearchInsert {
 
     }
 
+    public int search(int[] nums) {
+        int min = nums[0];
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i; j < nums.length; j++) {
+                min = Math.min(min, Math.min(nums[i], nums[j]));
+            }
+        }
+
+        return min;
+    }
+
     public static void main(String[] args) {
         SearchInsert si = new SearchInsert();
-        si.searchInsert(new int[]{1,3,5,6}, 5);
+        si.searchInsert(new int[]{1, 3, 5, 6}, 5);
+        System.out.println(si.search(new int[]{8,3,5,2,1243,1223,124,31239,32,12312,412312,521321456,5324324,123124,321421,21321125,536435,436346,345345234,4332432,532423432,5324234,235236234,3243283,4234234,23523423,4324,235234,23423523,23423,4235234,324236,23324,1523,1213,132,12,-2535,6,-5,32,12,421,321,532,213124,213124,32141,52423,123123,421125,536123,123124,4215123,1234}));
     }
 }
