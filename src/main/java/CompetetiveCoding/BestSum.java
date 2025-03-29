99e8ca1fa86fc0525457a0a96540ffd8146fcb80
Omer Isik <isikomer4545@gmail.com>
2025-03-29 12:41:16 +0000

    Coding exercise

--- src/main/java/CompetetiveCoding/BestSum.java
+++ src/main/java/CompetetiveCoding/BestSum.java
@@ -1,8 +1,6 @@
 package CompetetiveCoding;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class BestSum {
 
@@ -12,14 +10,20 @@ public class BestSum {
         int[] nums = new int[]{5,3,4,7};
         int[] nums2 = new int[]{1,4,5};
         int[] nums3 = new int[]{1,2,5,25};
+        int[] nums5 = new int[]{5,2,25,1};
         int[] nums4 = new int[]{2,3,5};
         System.out.println(Arrays.toString(bestSum(nums, 7)));
+        System.out.println(bestSumTabulation(nums, 7));
         memo.clear();
         System.out.println(Arrays.toString(bestSum(nums2, 8)));
+        System.out.println(bestSumTabulation(nums2, 8));
         memo.clear();
         System.out.println(Arrays.toString(bestSum(nums3, 100)));
+        System.out.println(bestSumTabulation(nums3, 100));
+        System.out.println(bestSumTabulation(nums5, 100));
         memo.clear();
         System.out.println(Arrays.toString(bestSum(nums4, 8)));
+        System.out.println(bestSumTabulation(nums4, 8));
         memo.clear();
     }
 
@@ -43,5 +47,33 @@ public class BestSum {
         }
         memo.put(target, shortestCombination);
         return shortestCombination;
+    }
+
+    private static List<Integer> bestSumTabulation(int[] nums, int target) {
+
+        List<List<Integer>> table = new ArrayList<>();
+        for (int i = 0; i <= target; i++) {
+            table.add(null);
+        }
+
+        table.set(0, new ArrayList<>());
+
+        for (int i = 0; i <= target; i++) {
+            if (table.get(i) != null) {
+                for (int num : nums) {
+                    int next = i + num;
+                    if (next <= target) {
+                        List<Integer> newCombination = new ArrayList<>(table.get(i));
+                        newCombination.add(num);
+                        if (table.get(next) == null || table.get(next).size() > newCombination.size()) {
+                            table.set(next, newCombination);
+                        }
+                    }
+                }
+            }
+        }
+
+        return table.get(target);
+
     }
 }
