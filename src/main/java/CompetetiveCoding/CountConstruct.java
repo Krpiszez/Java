db88790eedb6e5f54b20ec4cc6c68b7c308d6673
Omer Isik <isikomer4545@gmail.com>
2025-03-29 13:53:04 +0000

    Coding exercise

--- src/main/java/CompetetiveCoding/CountConstruct.java
+++ src/main/java/CompetetiveCoding/CountConstruct.java
@@ -11,14 +11,19 @@ public class CountConstruct {
         HashMap<String, Integer> memo = new HashMap<>();
 
         System.out.println(canConstruct.canConstruct(memo, "skateboard", new String[]{"bo", "rd", "ate", "t", "ska", "sk", "boar"}));
+        System.out.println(canConstruct.canConstructTabulation("skateboard", new String[]{"bo", "rd", "ate", "t", "ska", "sk", "boar"}));
         memo.clear();
         System.out.println(canConstruct.canConstruct(memo, "purple", new String[]{"purp", "p", "ur", "le", "purpl", "sk", "boar"}));
+        System.out.println(canConstruct.canConstructTabulation("purple", new String[]{"purp", "p", "ur", "le", "purpl", "sk", "boar"}));
         memo.clear();
         System.out.println(canConstruct.canConstruct(memo, "abcdef", new String[]{"ab", "abc", "cd", "def", "abcd"}));
+        System.out.println(canConstruct.canConstructTabulation("abcdef", new String[]{"ab", "abc", "cd", "def", "abcd"}));
         memo.clear();
         System.out.println(canConstruct.canConstruct(memo, "enterapotentpot", new String[]{"a", "p", "ent", "enter", "ot", "o", "t"}));
+        System.out.println(canConstruct.canConstructTabulation("enterapotentpot", new String[]{"a", "p", "ent", "enter", "ot", "o", "t"}));
         memo.clear();
         System.out.println(canConstruct.canConstruct(memo, "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef", new String[]{"e", "ee", "eee", "eeee", "eeeee", "eeeeee", "eeeeeee"}));
+        System.out.println(canConstruct.canConstructTabulation("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef", new String[]{"e", "ee", "eee", "eeee", "eeeee", "eeeeee", "eeeeeee"}));
         memo.clear();
     }
 
@@ -39,5 +44,25 @@ public class CountConstruct {
         }
         memo.put(abcdef, totalCount);
         return totalCount;
+    }
+
+    private int canConstructTabulation(String abcdef, String[] strings) {
+
+        int[] table = new int[abcdef.length() + 1];
+        table[0] = 1;
+
+        for (int i = 0; i <= abcdef.length(); i++) {
+            if (table[i] != 0) {
+                for (String str : strings) {
+                    if (i + str.length() <= abcdef.length() &&
+                            abcdef.substring(i, i + str.length()).equals(str)) {
+                        table[i + str.length()] += table[i];
+                    }
+                }
+            }
+        }
+
+
+        return table[abcdef.length()];
     }
 }
